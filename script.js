e52219bf0a6e31de0d48d97023707175b6d58de8
lapr4_1110637 <1110637@isep.ipp.pt>
2013-11-03 22:36:36 +0000

    Revert "ultimos updates"
    This reverts commit c8184541eba8e10242d376166a23a8457bf2bb1e.

--- script.js
+++ script.js
@@ -33,19 +33,15 @@ function CreateXmlHttpRequestObject( )
 			//vai buscar a categoria selecionada
 			var categoria = document.getElementById("categorias").childNodes[document.getElementById("categorias").selectedIndex + 2].firstChild.nodeValue;
 			var num = document.getElementById("numLivros").value;
-			if(!isNaN(num)){
-				xmlHttpObj.open("GET", "b.php?categoria=" + categoria + "&num=" + num, true);
-				xmlHttpObj.onreadystatechange = adicionaLivros;
-			}
+			
+			xmlHttpObj.open("GET", "b.php?categoria=" + categoria + "&num=" + num, true);
+			xmlHttpObj.onreadystatechange = adicionaLivros;
 		}
 		if(param === 2){
 			var n = document.getElementById("n_livros_editora").value;
-			if(!isNaN(n)){
-				xmlHttpObj.open("GET", "c.php?n=" + n, true);
+			xmlHttpObj.open("GET", "c.php?n=" + n, true);
 			
-				xmlHttpObj.onreadystatechange = listaNLivrosEditora;
-			}
-			document.getElementById("n_livros_editora").value = '';
+			xmlHttpObj.onreadystatechange = listaNLivrosEditora;
 		}
 		xmlHttpObj.send(null);
 	 }
@@ -120,15 +116,12 @@ function listaNLivrosEditora(){
 		
 		limpaLista("livros");
 		var editoras = xml.getElementsByTagName("editora");
-		var editora=document.getElementById("editoras").options;
-		var index = document.getElementById("editoras").selectedIndex;
-		
 		var length = editoras.length;//numero de editoras
-		//for(var i = 0; i < length; i++){
-			var livros = editoras[index].getElementsByTagName("title");
+		for(var i = 0; i < length; i++){
+			var livros = editoras[i].getElementsByTagName("title");
 			var len = livros.length;//numero de titulos numa editora
 			if(len != 0){
-				document.getElementById("livros").appendChild(document.createTextNode("Editora " + (index+1)));
+				document.getElementById("livros").appendChild(document.createTextNode("Editora " + (i+1)));
 				document.getElementById("livros").appendChild(document.createElement("br"));
 				for(var a=0;a<livros.length;a++){
 					var livro = livros[a].firstChild.nodeValue;
@@ -140,7 +133,7 @@ function listaNLivrosEditora(){
 					document.getElementById("livros").appendChild(document.createElement("br"));
 				}
 			}
-		//}		
+		}		
 	}
 }
 
@@ -175,7 +168,7 @@ function infoAdiciona(livro){
 				table.appendChild(row1);
 				table.appendChild(row2);
 			}
-		};
+		}
 		xmlHttpObj.open("GET","getInfo.php?livro="+livro,true);
 		xmlHttpObj.send();
 	}
